accd0b99d0d6ba5cddf274a0364487337c7b97da
Daniel Simpson <danielsimpson937@gmail.com>
2021-06-07 23:49:41 +1000

    Add abc204_a.py

--- atcoder/abc204/abc204_a.py
+++ atcoder/abc204/abc204_a.py
@@ -1,1 +1,11 @@
+def hand(x, y):
+    return x if x == y else {0, 1, 2}.difference({x, y}).pop()
 
+
+def main():
+    x, y = map(int, input().split())
+    print(hand(x, y))
+
+
+if __name__ == '__main__':
+    main()
