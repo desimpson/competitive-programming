5edf565bd3d4b0255d2c4bb8d37be01d1bb6a2a6
Daniel Simpson <danielsimpson937@gmail.com>
2021-06-08 21:52:38 +1000

    Add abc204_c.py

---
+++ atcoder/abc204/abc204_c.py
@@ -0,0 +1,42 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+class Graph:
+    def __init__(self):
+        self.graph = dict()
+
+    def add(self, u, v=None):
+        if u not in self.graph.keys():
+            self.graph[u] = set()
+        if v is None:
+            self.graph[u] = set() if v is None else self.graph[u].add(v)
+        else:
+            self.graph[u].add(v)
+
+    def dfs(self, v):
+        visited = set()
+        self.dfs_helper(v, visited)
+        return visited
+
+    def dfs_helper(self, v, visited):
+        visited.add(v)
+        [self.dfs_helper(neighbour, visited) for neighbour in self.graph[v]
+         if neighbour not in visited]
+
+
+def main():
+    N, M = map(int, input().split())
+    g = Graph()
+    [g.add(u) for u in range(N)]
+    for _ in range(M):
+        u, v = map(int, input().split())
+        g.add(u - 1, v - 1)
+
+    visited_count = sum([len(g.dfs(v)) for v in range(N)])
+    print(visited_count)
+
+
+if __name__ == '__main__':
+    main()
